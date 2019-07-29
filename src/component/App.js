3a4398fe390d8be860117e2c02644bb53fd8b48d
Eduardo Leones <eduardo.rodrigues@luizalabs.com>
2019-07-29 13:10:24 -0300

    create css in app

---
+++ src/component/App.js
@@ -0,0 +1,290 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          List View:
+        </p>
+
+        <div className="App-list">
+
+          <ul className="App-list__container">
+
+           {/* REPEATE ME */}
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li className="App-list__box">
+              <div className="App-list__persona">
+                <img className="App-list__image" src="https://uploads.metropoles.com/wp-content/uploads/2019/07/20165319/Screenshot_24190.jpg" alt="teste"/>
+              </div>
+              <div className="App-list__content">
+                <ul>
+                  {/* REPEATE ME */}
+                  <li>
+                    <span>Nome:</span>
+                    <span>FULANO</span>
+                  </li>
+                  <li>
+                    <span>Idade:</span>
+                    <span>24</span>
+                  </li>
+                  <li>
+                    <span>Sexo:</span>
+                    <span>MUITO</span>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            
+          </ul>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
