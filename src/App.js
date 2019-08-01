52d6406b2b722cf3bb36a5dab7d6108317c9d590
Eduardo Leones <eduardo.rodrigues@luizalabs.com>
2019-08-01 09:58:12 -0300

    added new ajax request

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import React, { Component } from 'react';
 import Contacts from './components/Contacts';
 import './App.css';
 
-const BACKEND_TOKEN = 'efed65f76270352a3bfbaf406644c8bb037a6f52'
+
+const url = 'https://reqres.in/api';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts: []
+      contacts: [],
+      users: [],
+      userId: ''
     };
   }
-  
+
+  handleusers = (identifier) => {
+    return (
+      fetch(`${url}/users/${identifier}`)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ 
+            users: data.data
+          })
+        })
+        .catch(console.log)
+    )
+  }
+
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/shoes/?token='+BACKEND_TOKEN)
+    fetch(`${url}/users/`)
       .then(res => res.json())
       .then((data) => {
         this.setState({ contacts: data.data })
       })
       .catch(console.log)
   }
+
   render() {
     return (
-      <Contacts contacts={this.state.contacts} />
+      <Contacts
+        contacts={this.state.contacts}
+        users={this.handleusers}
+        userId= {this.state.users}
+      />
     )
   }
-
 }
 
 export default App;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Contacts = ({ contacts }) => {
+const Contacts = ({ contacts, users, userId }) => {
     return (
         <div className="App">
 
@@ -8,23 +8,34 @@ const Contacts = ({ contacts }) => {
                 <ul className="App-list__container">
 
                     {contacts.map((contact) => (
-                        <li className="App-list__box">
+                        <li className="App-list__box" key={`list${contact.id}`}>
                             <div className="App-list__persona">
                                 <img className="App-list__image" src={contact.avatar} alt="imagen" />
                             </div>
                             <div className="App-list__content">
                                 <ul className="App-list__content-card">
                                     <li className="App-list__content-informations">
-                                        <span className="App-list__title">MARCA:</span>
-                                        <span className="App-list__data">{contact.brand}</span>
+                                        <span className="App-list__title">Nome:</span>
+                                        <span className="App-list__data">{contact.first_name}</span>
                                     </li>
                                     <li className="App-list__content-informations">
-                                        <span className="App-list__title">MODELO:</span>
-                                        <span className="App-list__data">{contact.model}</span>
+                                        <span className="App-list__title">Sobrenome:</span>
+                                        <span className="App-list__data">{contact.last_name}</span>
                                     </li>
-                                    <li className="App-list__content-informations">
-                                        <span className="App-list__title">PREÇO:</span>
-                                        <span className="App-list__data">R$: {contact.price}</span>
+                                    {/* <li className="App-list__content-informations">
+                                        <span className="App-list__title">Email:</span>
+                                        <span className="App-list__data">{contact.email}</span>
+                                    </li> */}
+                                    <li>
+                                        <button onClick={() => users(contact.id)}>Ver Email</button>
+                                        {
+                                            contact.id === userId.id  
+                                            ?   <li className="App-list__content-informations">
+                                                    <span className="App-list__title">Email:</span>
+                                                    <span className="App-list__data">{userId.email}</span>
+                                                </li>
+                                            : null 
+                                        }
                                     </li>
                                 </ul>
                             </div>
